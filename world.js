a22876ec048fc7f5c93c50fd05270c860b872fa1
Staddy <stas.ponkrashov@gmail.com>
2016-01-03 04:27:40 +0300

    Level collision check

--- world.js
+++ world.js
@@ -1,5 +1,48 @@
 var WORLD = {};
 
+WORLD.entities = [];
+WORLD.enemies = [];
+WORLD.playerBullets = [];
+WORLD.enemyBullets = [];
+WORLD.player = {};
+
+WORLD.loadLevel = function() {
+    WORLD.X = 80;
+    WORLD.Y = 60;
+    WORLD.TILE = 10;
+    WORLD.blocks = new Array(WORLD.X);
+    for(var i = 0; i < WORLD.X; ++i) {
+        WORLD.blocks[i] = new Array(WORLD.Y);
+        for(var j = 0; j < WORLD.Y; ++j)
+            WORLD.blocks[i][j] = 0;
+    }
+}
+
+WORLD.isBlocked = function(x, y, width, height) {
+    var x1 = Math.floor(x / WORLD.TILE);
+    if(x1 < 0) x1 = 0;
+
+    var y1 = Math.floor(y / WORLD.TILE);
+    if(y1 < 0) y1 = 0;
+
+    var mx = (x + width) / WORLD.TILE;
+    var x2 = Math.floor(mx);
+    if((mx) == Math.floor(mx)) --x2;
+    if(x2 >= WORLD.X) x2 = WORLD.X - 1;
+
+    var my = (y + height) / WORLD.TILE;
+    var y2 = Math.floor(my);
+    if((my) == Math.floor(my)) --y2;
+    if(y2 >= WORLD.X) y2 = WORLD.Y - 1;
+
+    for(var i = x1; i < x2; ++i)
+        for(var j = y1; j < y2; ++j)
+            if(WORLD.blocks[i][j] == 1)
+                return true;
+
+    return false;
+}
+
 WORLD.areCollide = function(r1, r2) {
     //Define the variables we'll need to calculate
     var hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
@@ -43,4 +86,37 @@ WORLD.areCollide = function(r1, r2) {
     }
     //`hit` will be either `true` or `false`
     return hit;
+}
+
+WORLD.tick = function() {
+    WORLD.enemyBullets.forEach(
+        function(b) {
+            if(WORLD.areCollide(b, WORLD.player)) {
+                WORLD.player.collide(b);
+                b.collide();
+            }
+        }
+    );
+    WORLD.playerBullets.forEach(
+        function(b) {
+            WORLD.enemies.forEach(
+                function(e) {
+                    if(WORLD.areCollide(b, e)) {
+                        e.collide(b);
+                        b.collide(e);
+                    }
+                }
+            )
+        }
+    )
+    WORLD.player.tick();
+    for(var i = 0; i < WORLD.entities.length; ++i) {
+        var e = entities[i];
+        if(!e.removed)
+            e.tick();
+        else {
+            WORLD.entities.splice(i--, 1);
+            e.category.remove(e);
+        }
+    }
 }
